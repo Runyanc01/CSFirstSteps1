9a7dee779b40d1cbe4627f0724f5926c785e27e3
Runyanc01 <corunyan01@gmail.com>
2024-04-09 09:37:07 -0400

    Rework binary game

--- src/main/java/com/csfirststeps/application/views/datarep/binarygame/BinaryGameDriver.java
+++ src/main/java/com/csfirststeps/application/views/datarep/binarygame/BinaryGameDriver.java
@@ -1,28 +1,29 @@
 package com.csfirststeps.application.views.datarep.binarygame;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 @Tag("div")
 public class BinaryGameDriver extends VerticalLayout{
-
-    private Timer timer;
-
-    Button startButton;
-
-    Button gameOverButton;
 
     Div gameArea = new Div();
 
     Div gameMenu = new Div();
 
-    int rowCount = 0;
+    Button start = new Button("Start");
+
+    Button stop = new Button("End Game");
+
+    boolean gameRunning = false;
+
+    int binary;
+
+    int num;
 
     public BinaryGameDriver() {
 
@@ -32,43 +33,40 @@ public class BinaryGameDriver extends VerticalLayout{
                 .set("width", "600px")
                 .set("padding-left", "18px");
 
-        gameOverButton = new Button("Play Again");
-        gameOverButton.getStyle()
-                .setHeight("50px")
-                .setWidth("200px")
-                .setBackgroundColor("#892d2d")
-                .setColor("black");
-        gameOverButton.setEnabled(false);
+        gameMenu.getStyle()
+                .set("border-radius", "5px")
+                .set("padding", "12px")
+                .set("height", "66px")
+                .set("width", "600px")
+                .set("display", "flex")
+                .set("flex-direction", "row")
+                .set("justify-content", "center");
 
-        startButton = new Button("Start");
-        startButton.getStyle()
-                .setHeight("50px")
-                .setWidth("200px")
-                .setBackgroundColor("#3a6d24")
-                .setColor("white");
-
-        startButton.addClickListener(event -> startGame());
+        start.getStyle()
+                .set("padding", "3px")
+                .set("height", "60px")
+                .set("min-width", "250px");
+        start.setDisableOnClick(true);
+        start.addClickListener(event -> {
+            run();
+            start.setEnabled(false);
+        });
+        stop.getStyle()
+                .set("padding", "3px")
+                .set("height", "60px")
+                .set("min-width", "250px");
+        stop.addClickListener(event -> end());
+        gameMenu.add(start, stop);
 
         gameArea.getStyle()
-                // .set("border", "1.25px solid black")
                 .set("border-radius", "5px")
                 .set("padding", "0px")
-                .set("height", "525px")
+                .set("height", "66px")
                 .set("width", "600px")
                 .set("display", "flex")
                 .set("flex-direction", "column-reverse")
                 .set("align-items", "flex-start");
 
-        gameMenu.getStyle()
-                .set("border", "1.25px border")
-                .set("border-radius", "5px")
-                .set("padding", "0")
-                .set("height", "90px")
-                .set("width", "600px")
-                .set("display", "flex")
-                .set("justify-content", "center");
-
-        gameMenu.add(startButton);
 
 
 
@@ -76,111 +74,46 @@ public class BinaryGameDriver extends VerticalLayout{
 
     }
 
-    public void startGame() {
-        startButton.setEnabled(false);
-        startButton.setVisible(false);
-
-        startPolling();
-
-        generateFirstRow();
-    }
-
-    private void startPolling() {
-
-        UI.getCurrent().addPollListener(event -> handlePolling());
-
-        UI.getCurrent().setPollInterval(5000);
-
-    }
-
-    private void stopPolling() {
-        if (timer != null) {
-            timer.cancel();
-            timer = null;
-            UI.getCurrent().setPollInterval(-1);
-        }
-
-        gameOverButton.setEnabled(true);
-    }
-
-    private void handlePolling() {
-        UI.getCurrent().access(() -> {
-           startGameTimer();
-        });
-    }
-
-    private void startGameTimer() {
-      final int intervalSeconds = 35;
-
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                getUI().ifPresent(ui -> ui.access(() -> createGame()));
-            }
-        }, 0, intervalSeconds * 1000);
-    }
 
 
-    public void generateFirstRow () {
 
-        Row row = new Row(this::removeRowIfMatch);
-        gameArea.getElement().insertChild(0,row.getElement());
-        rowCount++;
-        UI.getCurrent().push();
-
-    }
-
-
-    public void createGame () {
-
-        if (rowCount < 12) {
-
-            Row row = new Row(this::removeRowIfMatch);
-            gameArea.getElement().insertChild(0, row.getElement());
-            rowCount++;
-            UI.getCurrent().push();
-
-
-        } else {
-            stopPolling();
-            gameArea.removeAll();
-            gameArea.getStyle()
-                    //.set("background-color", "#313131")
-                    .set("justify-content", "center")
-                    .set("align-items", "center");
-            gameArea.add(gameOverButton);
-            gameArea.setEnabled(true);
-            gameOverButton.addClickListener(event -> {
-                rowCount = 0;
-                gameArea.getStyle()
-                        .set("background-color", "transparent")
-                        .set("display", "flex")
-                        .set("justify-content", "flex-start")
-                        .set("flex-direction", "column-reverse")
-                        .set("align-items", "flex-start");
-                gameArea.removeAll();
-                restartGame();
-            });
-        }
-    }
 
     public void removeRowIfMatch(Row row) {
         int sum = row.getButtonValuesSum();
         int targetValue = row.getDecimalValue();
         if (sum == targetValue) {
             gameArea.getElement().removeChild(row.getElement());
-            rowCount--;
+
         }
     }
 
-    public void restartGame() {
-        startButton.setEnabled(false);
-        startButton.setVisible(false);
-        if (timer == null) {
-            startPolling();
-            generateFirstRow();
-        }
+    public void populateRow () {
+
+            Row row = new Row();
+            gameArea.getElement().insertChild(0, row.getElement());
+            UI.getCurrent().push();
+
+    }
+
+    public void run() {
+
+        Row row = new Row();
+        gameArea.add(row);
+        UI.getCurrent().push();
+        removeRowIfMatch(row);
+        
+
+    }
+
+    public void end() {
+        gameArea.removeAll();
+        start.setEnabled(true);
     }
 
 }
+
+
+
+
+
+
--- src/main/java/com/csfirststeps/application/views/datarep/binarygame/Row.java
+++ src/main/java/com/csfirststeps/application/views/datarep/binarygame/Row.java
@@ -1,10 +1,9 @@
 package com.csfirststeps.application.views.datarep.binarygame;
 
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.textfield.NumberField;
+
 
 import java.util.Random;
-import java.util.function.Consumer;
 
 public class Row extends Span {
 
@@ -13,12 +12,9 @@ public class Row extends Span {
 
     private CustGameButton[] buttons = new CustGameButton[8];
 
-    private Consumer<Row> rowRemovalCallback;
 
+    public Row() {
 
-    public Row(Consumer<Row> rowRemovalCallback) {
-        
-        this.rowRemovalCallback = rowRemovalCallback;
         this.random = new Random();
 
 
@@ -39,7 +35,6 @@ public class Row extends Span {
             buttons[i] = new CustGameButton(values, i);
             buttons[i].getStyle().set("margin-left", "8px");
             buttons[i].getStyle().set("margin-right", "8 px");
-            buttons[i].addClickListener(event -> checkAndRemoveRow(targetNum));
             container.add(buttons[i]);
         }
 
@@ -52,13 +47,7 @@ public class Row extends Span {
 
     }
 
-    private void checkAndRemoveRow(int targetNum) {
-        if (rowRemovalCallback != null) {
-            if (getButtonValuesSum() == targetNum) {
-                rowRemovalCallback.accept(this);
-            }
-        }
-    }
+
 
     public int getButtonValuesSum () {
         int sum = 0;
@@ -71,10 +60,8 @@ public class Row extends Span {
     }
 
     public int getDecimalValue() {
-        randNum = random.nextInt(255) +1;
+        randNum = random.nextInt(128) +1;
         return randNum;
     }
-
-
 
 }
